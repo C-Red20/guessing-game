94b0ddd7ed6b3437319c43e20b84e93333062299
C-Red20 <caila.linger@yahoo.com>
2024-07-17 15:33:55 -0400

    Phase 1 in

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
 using System;
 
-Console.WriteLine("Welcome to the Guessing Game!");
-Console.WriteLine("Try to guess the secret number.");
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the Guessing Game!");
+        Console.WriteLine("Try to guess the secret number.");
+
+        Console.Write("Enter your guess: ");
+        string userInput = Console.ReadLine();
+        int userGuess = Convert.ToInt32(userInput);
+
+        Console.WriteLine($"You guessed: {userGuess}");
+    }
+}
