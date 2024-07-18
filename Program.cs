9c8735d2ab0ab6acc069ac49873c8e92b60c64f8
C-Red20 <caila.linger@yahoo.com>
2024-07-18 10:57:03 -0400

    Phase 5

--- Program.cs
+++ Program.cs
@@ -7,14 +7,15 @@ class Program
         Console.WriteLine("Welcome to the Guessing Game!");
         Console.WriteLine("Try to guess the secret number. (Between 1 & 100)");
 
-        int secretNumber = 42;
+        Random random = new Random();
+        int secretNumber = random.Next(1, 101);
         int guessesLeft = 4;
         int guessesMade = 0;
 
         while (guessesLeft > 0)
         {
             guessesMade++;
-            Console.Write($"Your guess ({guessesMade}): ");
+            Console.Write($"Your guess ({guessesMade}): , guesses left: {guessesLeft}: ");
             int userGuess = Convert.ToInt32(Console.ReadLine());
 
             if (userGuess == secretNumber)
